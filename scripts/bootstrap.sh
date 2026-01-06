dbe6b4e011cebac6f4e1b6db5307f6cd7d3ce35a
cjn4825 <Christian4825@gmail.com>
2026-01-06 14:21:27 -0600

    forgot to uppercase the  usage

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,9 +7,9 @@ set -e
 
 DOTFILES="$HOME/.dotfiles"
 
-ln -sf "$Dotfiles/bash/.bashrc.d/prompt.sh" "$HOME/.bashrc.d/prompt.sh"
-ln -sf "$Dotfiles/nvim" "$HOME/.config/nvim"
-ln -sf "$Dotfiles/tmux/.tmux" "$HOME/.tmux"
-ln -sf "$Dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DOTFILES/bash/.bashrc.d/prompt.sh" "$HOME/.bashrc.d/prompt.sh"
+ln -sf "$DOTFILES/nvim" "$HOME/.config/nvim"
+ln -sf "$DOTFILES/tmux/.tmux" "$HOME/.tmux"
+ln -sf "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
 echo "Dotfiles linked!"
